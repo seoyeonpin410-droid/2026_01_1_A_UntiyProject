82280dfc32d5a1755a671445889869c91a827ed7
seoyeonpin410-droid <seoyeonpin410@gmail.com>
2026-04-16 12:08:28 +0900

    [Feat]플레이어 개선, 물리 마찰, 점프 개선, 코요테 타임, 글라이딩

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,43 +2,133 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("기본 이동 설정")]
     public float moveSpeed = 5.0F;
     public float JumpForce = 5.0F;
+    public float turnSpeed = 10.0f;
+
+    [Header("점프 개선 설정")]
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2.0f;
+
+    [Header("지면 감지 설정")]
+    public float coyoteTime = 0.15f;
+    public float coyoteTimeCounter;
+    public bool isGrounded = true;
+
+
+    [Header("글라인더 설정")]
+    public GameObject gliderObject;
+    public float gliderFallSpeed = 1.0f;
+    public float gliderFMoveSpeed = 7.0f;
+    public float gliderMaxTime = 5.0f;
+    public float gliderTimeLeft;
+    public bool isGliding = false;
+
 
     public Rigidbody rb;
-    public bool isGrounded = true;
+    public bool realGrounded = true;
 
     public int coinCount = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        coyoteTimeCounter = 0;
 
+        if (gliderObject != null)
+        {
+            gliderObject.SetActive(false);
+        }
+
+        gliderTimeLeft = gliderMaxTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = realGrounded;
 
+        UpdateGroundedState();
+        //움직임
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        rb.linearVelocity = new Vector3(moveHorizontal * moveSpeed, rb.linearVelocity.y, moveVertical * moveSpeed);
+        //이동 방향 백터
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
+
+        if (movement.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        }
+
+        //G키로 글라인더 제어 (누르는 동안 활성화)
+        if (Input.GetKey(KeyCode.G) && !isGrounded && gliderTimeLeft > 0)
+        {
+            if (!isGrounded)
+            {
+                EnableGlider();
+
+            }
+            gliderTimeLeft -= Time.deltaTime;
+            
+            if (gliderTimeLeft <= 0)
+            {
+                DisableGlider();
+            }
+        }
+        else if (isGrounded)
+        {
+            DisableGlider();
+        }
+
+        if (isGliding)
+        {
+            ApplyGliderMovement(moveHorizontal, moveVertical);
+        }
+        else
+        {
+            //강체 속도값 이동 
+            rb.linearVelocity = new Vector3(moveHorizontal * moveSpeed, rb.linearVelocity.y, moveVertical * moveSpeed);
+
+            //착시 점프 높이 구현
+            if (rb.linearVelocity.y < 0)
+            {
+                rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            }
+            else if (rb.linearVelocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.linearVelocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            }
+
+           
+        }
+
+       
+
+        //점프 입력
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             isGrounded = false;
+            realGrounded = false;
+            coyoteTimeCounter = 0;
+        }
+        //지면에 있으면 글라이더 시간 회복 및 글라이더 비활성화
+        if (isGrounded)
+        {
+            if (isGliding)
+            {
+                DisableGlider();
+            }
+            gliderTimeLeft = gliderMaxTime; //지상에 있을땐 시간 회복
+
         }
 
     }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
+   
 
 
     void OnTriggerEnter(Collider other)
@@ -49,6 +139,84 @@ public class PlayerMovement : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            realGrounded = true;
+        }
+    }
 
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            realGrounded = true;
+        }
+    }
 
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            realGrounded = false;
+        }
+    }
+
+    void UpdateGroundedState()
+    {
+
+        if (realGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+            isGrounded = true ;
+        }
+        else
+        {
+           if(coyoteTimeCounter > 0)
+            {
+                coyoteTimeCounter -= Time.deltaTime;
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+        }
+    }
+
+    //글라인더 활성화 함수
+    void EnableGlider()
+    {
+        isGliding = true ;
+
+        if (gliderObject !=null)
+        {
+            gliderObject.SetActive(true);
+        }
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, -gliderFallSpeed, rb.linearVelocity.z);
+    }
+
+    void DisableGlider()
+    {
+        if (!isGliding) return;
+
+        isGliding = false ;
+        if (gliderObject != null)
+        {
+            gliderObject.SetActive(false);
+        }
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+    }
+
+    //글라인더 이동 적용
+
+    void ApplyGliderMovement(float horizontal, float vertical)
+    {
+        Vector3 gliderVelocity = new Vector3(horizontal * gliderFMoveSpeed, -gliderFallSpeed, vertical * gliderFMoveSpeed);
+
+        rb.linearVelocity = gliderVelocity;
+    }
 }
